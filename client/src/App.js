674cb7abdd1174eb2ef94387be248736fa7d35b4
Ariel Slavinsky <nadav8.tch15@gmail.com>
2022-09-01 10:21:30 +0300

    finished song page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
+import React from "react";
+import SiteHeader from "./components/SiteHeader";
+import SiteFooter from "./components/SiteFooter";
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+
 function App() {
   return (
-    <></>
+    <div>
+      <SiteHeader page="Home" />
+      <Routes>
+        <Route to="About" element={<About />} />
+      </Routes>
+      
+      <SiteFooter />
+    </div>
   );
 }
 
