93ae2b48c07fe057f968df51971d1467091ddac7
roee navon <nadav.tch16@gmail.com>
2022-09-01 16:19:57 +0300

    fix songurl

--- client/src/Services/utils.js
+++ client/src/Services/utils.js
@@ -1,19 +1,10 @@
-import dumb from "../Assets/audioFiles/dumb.mp3";
-import ivriAnochi from "../Assets/audioFiles/ivriAnochi.mp3";
-import neverGonnaGiveYouUp from "../Assets/audioFiles/neverGonnaGiveYouUp.mp3";
-import ourGodIsAwsomeGod from "../Assets/audioFiles/ourGodIsAwsomeGod.mp3";
-import trapAnthem from "../Assets/audioFiles/trapAnthem.mp3";
-const songs = {
-  "dumb.mp3": dumb,
-  "ivriAnochi.mp3": ivriAnochi,
-  "neverGonnaGiveYouUp.mp3": neverGonnaGiveYouUp,
-  "ourGodIsAwsomeGod.mp3": ourGodIsAwsomeGod,
-  "trapAnthem.mp3": trapAnthem
-};
 let audio = null;
-const playSong = async (songUrl) => {
+const playSong = (songUrl) => {
   audio?.pause();
-  audio = new Audio(songs[songUrl]);
+  audio = new Audio(songUrl);
   audio.play();
 };
-export { playSong };
+const stopSong = () => {
+  audio?.pause();
+}
+export { playSong,stopSong };
