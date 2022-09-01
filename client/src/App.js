368f9ea19c9da7c224097d9d12f62b4a578b9029
roee navon <nadav.tch16@gmail.com>
2022-09-01 09:48:20 +0300

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import SiteHeader from "./components/SiteHeader";
 import SiteFooter from "./components/SiteFooter";
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/About";
+import Home from "./pages/Home";
+import SongPage from "./pages/SongPage";
 
 function App() {
   return (
     <div>
       <SiteHeader page="Home" />
       <Routes>
-        <Route to="About" element={<About />} />
+        <Route path="About" element={<About />} />
+        <Route path="/" element = {<Home />} />
+        <Route path="/Song/:id" element = {<SongPage/>}/>
       </Routes>
       
       <SiteFooter />
--- client/src/components/SongList.jsx
+++ client/src/components/SongList.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import SongPreview from './SongPreview'
+import { getAllSongs } from '../Services/songService'
 
 export default class SongList extends Component {
-    constructor(props){
+    constructor(props){+
         super(props)
         this.state = {songs:[{id:'012345',name:'test',}]}
     }
     componentDidMount(){
+        this.setState({songs:getAllSongs().then()})
 
     }
   render() {
