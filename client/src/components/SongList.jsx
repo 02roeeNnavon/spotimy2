f65a4c9bb39217a07494aef91aa38673cd9349dd
Ariel Slavinsky <nadav8.tch15@gmail.com>
2022-09-04 13:59:02 +0300

    fixed songlist

--- client/src/components/SongList.jsx
+++ client/src/components/SongList.jsx
@@ -1,23 +1,15 @@
 import React, { Component } from 'react'
 import SongPreview from './SongPreview'
-import { getAllSongs } from '../Services/songService.js'
 import { stopSong } from '../Services/utils'
 
 export default class SongList extends Component {
-    constructor(props){
-        super(props)
-        this.state = {songs:[{id:'012345',name:'test',}]}
-    }
-    async componentDidMount(){
-        this.setState({songs:await getAllSongs()})
-    }
-    componentWillUnmount(){
-      stopSong()
+    componentWillUnmount() {
+      stopSong();
     }
   render() {
     return (
-      <div>{this.state?.songs?.map((element) => {
-        return <SongPreview key={element.id} song={element} goToSongPage={(id) => {console.log('hello');}} ></SongPreview>
+      <div>{this.props.songs.map((element) => {
+        return <SongPreview key={element.id} song={element}></SongPreview>
       })}</div>
     )
   }
