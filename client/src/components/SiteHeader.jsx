aba3e4fad4ca95e8081369b7eb9766c834976b4a
roee navon <nadav.tch16@gmail.com>
2022-09-06 15:55:02 +0300

    auto-update

--- client/src/components/SiteHeader.jsx
+++ client/src/components/SiteHeader.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import LoginModal from "./LoginModal";
 import { loadFromStorage } from "../Services/LocalService";
-import { useState } from "react";
+import React, { Component } from "react";
 const user = (props) => {
   if (loadFromStorage("user")?.status === "admin") {
     return (
@@ -14,43 +14,53 @@ const user = (props) => {
     );
   }
 };
-export default function SiteHeader(props) {
-  return (
-    <nav className="navbar navbar-expand navbar-light bg-header gap">
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav d-flex align-items-center">
-          <Link className="nav-link text-defult" to="/">
-            <img
-              width="48"
-              height="48"
-              src="/Assets/logos/quacksounds-v5.svg"
-            />
-          </Link>
-          <Link className="nav-link text-defult" to="/">
-            <li className="nav-item active ml-2">
-              <h4>
-                {props.page == "home" ? <b>Spotimy</b> : "Spotimy"}{" "}
-                <span className="sr-only"></span>
-              </h4>
-            </li>
-          </Link>
 
-          <li className="nav-item active">
-            <Link className="nav-link text-defult" to="/About">
-              {props.page == "about" ? <b>About</b> : "About"}{" "}
-              <span className="sr-only"></span>
+export default class SiteHeader extends Component {
+    constructor(props){
+        super(props)
+        this.state = {user:{}}
+    }
+    update = () => {
+        this.setState({user:loadFromStorage("user")})
+    }
+  render() {
+    return (
+      <nav className="navbar navbar-expand navbar-light bg-header gap">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav d-flex align-items-center">
+            <Link className="nav-link text-defult" to="/">
+              <img
+                width="48"
+                height="48"
+                src="/Assets/logos/quacksounds-v5.svg"
+              />
             </Link>
-          </li>
-          <li className="nav-item active">
-            <Link className="nav-link text-defult" to="/SongLiked">
-              {props.page == "likedSongs" ? <b>LikesSongs</b> : "Liked Songs"}{" "}
-              <span className="sr-only"></span>
+            <Link className="nav-link text-defult" to="/">
+              <li className="nav-item active ml-2">
+                <h4>
+                  {this.props.page == "home" ? <b>Spotimy</b> : "Spotimy"}{" "}
+                  <span className="sr-only"></span>
+                </h4>
+              </li>
             </Link>
-          </li>
-          {user(props)}
-        </ul>
-      </div>
-      <LoginModal ></LoginModal>
-    </nav>
-  );
+
+            <li className="nav-item active">
+              <Link className="nav-link text-defult" to="/About">
+                {this.props.page == "about" ? <b>About</b> : "About"}{" "}
+                <span className="sr-only"></span>
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link className="nav-link text-defult" to="/SongLiked">
+                {this.props.page == "likedSongs" ? <b>LikesSongs</b> : "Liked Songs"}{" "}
+                <span className="sr-only"></span>
+              </Link>
+            </li>
+            {user(this.props)}
+          </ul>
+        </div>
+        <LoginModal update={this.update}></LoginModal>
+      </nav>
+    );
+  }
 }
